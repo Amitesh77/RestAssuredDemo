3be95f37587695de22237d6af2825b49bbd23f54
Amitesh77 <53990426+Amitesh77@users.noreply.github.com>
2020-02-13 19:03:34 +0530

    GetHeaders and Query Param Scenario added

--- com.ApiRest/src/test/java/com/API/com/ApiRest/City.java
+++ com.ApiRest/src/test/java/com/API/com/ApiRest/City.java
@@ -40,5 +40,45 @@ public class City extends Base{
 		
 		System.out.println("The wind speed is :"+wind);
 	}
+	
+	@Test
+	public void getHeaders() throws IOException
+	{
+		String baseuri=readPropertiesFile("host");
+		
+		response=RestAssured.given()
+				.baseUri(baseuri)
+				.basePath("utilities/weather/city/Hyderabad")
+				.get();
+		
+		Headers allheaders=response.headers();
+		
+		for (io.restassured.http.Header header : allheaders)
+		{
+			
+			 System.out.println("Key: " + header.getName() + " Value: " + header.getValue());
+			
+		}
+
+	}
+	
+	@Test
+	public void queryparam() throws IOException
+	{
+		String baseuri=readPropertiesFile("host1");
+		
+		response=RestAssured.given()
+				.baseUri(baseuri)
+				.queryParam("q","London,UK")
+				.queryParam("appid", "2b1fd2d7f77ccf1b7de9b441571b39b8")
+				.basePath("/weather")
+				.get();
+		
+		String Sbody=response.getBody().prettyPrint();
+		
+		System.out.println(Sbody);
+				
+				
+	}
 
 }
